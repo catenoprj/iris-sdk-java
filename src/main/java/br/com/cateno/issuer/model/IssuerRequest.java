0f066159189aacbe4c9105d77c7af10512129a9d
Cainã Ferreira da Silva <caina.ferreira@sensedia.com>
2019-02-15 09:28:03 -0200

    remove comment unnecessary

--- src/main/java/br/com/cateno/issuer/model/IssuerRequest.java
+++ src/main/java/br/com/cateno/issuer/model/IssuerRequest.java
@@ -9,17 +9,6 @@ public class IssuerRequest {
     private String name;
     private String issuerCode;
     private ExtraInfoRequest extraInfo;
-
-    /*public IssuerRequest() {
-        super();
-    }
-
-    public IssuerRequest(String issuerCode, String cnpj, String name, Status status, ExtraInfo extraInfo) {
-        this.issuerCode = issuerCode;
-        this.cnpj = cnpj;
-        this.name = name;
-        this.extraInfo = extraInfo;
-    }*/
 }
 
 
