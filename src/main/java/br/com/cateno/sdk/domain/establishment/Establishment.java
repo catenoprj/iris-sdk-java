da6d38b879b67c421b06f9c092d5fd78ef42b8c8
Cainã Ferreira da Silva <caina.ferreira@sensedia.com>
2019-02-18 16:39:18 -0300

    remove lombok

--- src/main/java/br/com/cateno/sdk/domain/establishment/Establishment.java
+++ src/main/java/br/com/cateno/sdk/domain/establishment/Establishment.java
@@ -1,14 +1,51 @@
 package br.com.cateno.sdk.domain.establishment;
 
-import lombok.Data;
-
 import java.util.UUID;
 
-@Data
 public class Establishment {
     private UUID id;
     private String cnpj;
     private ExtraInfo extraInfo;
     private String name;
     private Status status;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public ExtraInfo getExtraInfo() {
+        return extraInfo;
+    }
+
+    public void setExtraInfo(ExtraInfo extraInfo) {
+        this.extraInfo = extraInfo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 }
--- src/main/java/br/com/cateno/sdk/domain/establishment/ExtraInfo.java
+++ src/main/java/br/com/cateno/sdk/domain/establishment/ExtraInfo.java
@@ -1,10 +1,31 @@
 package br.com.cateno.sdk.domain.establishment;
 
-import lombok.Data;
-
-@Data
 public class ExtraInfo {
     private String mainPhone;
     private String backupPhone;
     private String email;
+
+    public String getMainPhone() {
+        return mainPhone;
+    }
+
+    public void setMainPhone(String mainPhone) {
+        this.mainPhone = mainPhone;
+    }
+
+    public String getBackupPhone() {
+        return backupPhone;
+    }
+
+    public void setBackupPhone(String backupPhone) {
+        this.backupPhone = backupPhone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
