f102c555133f70280f244a82baa6ad8a150c59f4
Tiago Angelo <tiago.angelo@sensedia.com>
2019-02-20 09:45:30 -0300

    Removing unused imports

--- src/main/java/br/com/cateno/sdk/core/IrisClient.java
+++ src/main/java/br/com/cateno/sdk/core/IrisClient.java
@@ -1,6 +1,5 @@
 package br.com.cateno.sdk.core;
 
-import br.com.cateno.sdk.domain.auth.AuthService;
 import br.com.cateno.sdk.domain.establishment.EstablishmentService;
 import br.com.cateno.sdk.domain.issuer.IssuerService;
 import dagger.Component;
