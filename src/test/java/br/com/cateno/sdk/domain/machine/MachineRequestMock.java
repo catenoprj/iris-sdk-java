0cdb9aaaa7534900ddb7c6054511e767f01eca1c
Guilherme Rogatto <gui.rogatto@gmail.com>
2019-02-21 15:18:59 -0300

    Unit Test for Machine resource

--- src/test/java/br/com/cateno/sdk/domain/machine/MachineRequestMock.java
+++ src/test/java/br/com/cateno/sdk/domain/machine/MachineRequestMock.java
@@ -1,26 +1,37 @@
 package br.com.cateno.sdk.domain.machine;
 
 import br.com.cateno.sdk.domain.establishment.*;
-import br.com.cateno.sdk.util.CNPJCreator;
+import br.com.cateno.sdk.util.AuthenticatedStageEnvTest;
 import com.github.javafaker.service.FakeValuesService;
 import com.github.javafaker.service.RandomService;
 
+import java.io.IOException;
 import java.util.Locale;
 
-public class MachineRequestMock {
+public class MachineRequestMock implements AuthenticatedStageEnvTest {
 
-        MachineRequest machineMock(){
+        MachineRequest machineRequestMock() throws IOException {
+
+        EstablishmentApiClient establishmentApiClient = this.getAuthenticatedRetrofit().create(EstablishmentApiClient.class);
+        EstablishmentService establishmentService = new EstablishmentService(establishmentApiClient);
+
+        EstablishmentRequestMock establishmentMock = new EstablishmentRequestMock();
+        Establishment establishmentCreateResponse = establishmentService.create(establishmentMock.establismentRequestMock());
+
 
         FakeValuesService fakeValuesService = new FakeValuesService(
                 new Locale("pt-BR"), new RandomService());
 
-                EstablishmentRequestMock establishmentMock = new EstablishmentRequestMock();
 
-                Establishment establishmentCreateResponse = service.create(establishmentMock.establismentRequestMock());
+        String number = fakeValuesService.regexify("[1-9]{6}");
+        String label = "11" + fakeValuesService.bothify("Machine????##");
 
         MachineRequest machineRequest = new MachineRequest();
 
-        machineRequest.setEstablishmentId();
+        machineRequest.setEstablishmentId(establishmentCreateResponse.getId());
+        machineRequest.setLabel(label);
+        machineRequest.setNumber(number);
+
         return machineRequest;
     }
 }
--- src/test/java/br/com/cateno/sdk/domain/machine/MachineServiceTest.java
+++ src/test/java/br/com/cateno/sdk/domain/machine/MachineServiceTest.java
@@ -1,5 +1,114 @@
 package br.com.cateno.sdk.domain.machine;
 
-public class MachineServiceTest {
+import br.com.cateno.sdk.domain.establishment.Machine;
+import br.com.cateno.sdk.domain.establishment.MachineApiClient;
+import br.com.cateno.sdk.domain.establishment.MachineRequest;
+import br.com.cateno.sdk.domain.establishment.MachineService;
+import br.com.cateno.sdk.util.AuthenticatedStageEnvTest;
+import com.github.javafaker.service.FakeValuesService;
+import com.github.javafaker.service.RandomService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Given a Machines service")
+class MachineServiceTest implements AuthenticatedStageEnvTest {
+
+    private final MachineService service;
+
+    MachineServiceTest() {
+        final MachineApiClient machineApiClient = this.getAuthenticatedRetrofit().create(MachineApiClient.class);
+        this.service = new MachineService(machineApiClient);
+    }
+
+    @Nested
+    @DisplayName("When create a new Machine")
+    class WhenCreateNewMachine {
+
+        @Test
+        @DisplayName("Then return the resource Machine generated with Id")
+        void thenReturnTheResourceMachineGeneratedWithId() throws IOException {
+            MachineRequestMock machineMock = new MachineRequestMock();
+
+            Machine machineCreateResponse = service.create(machineMock.machineRequestMock());
+            assertThat(machineCreateResponse.getId()).isNotNull();
+        }
+    }
+
+    @Nested
+    @DisplayName("When search a new Machine by your ID")
+    class WhenSearchNewMachineById {
+
+        @Test
+        @DisplayName("Then return the related resource with the same ID")
+        void thenReturnTheRelatedResourceWithTheSameId() throws IOException {
+            MachineRequestMock machineMock = new MachineRequestMock();
+
+            Machine machineCreateResponse = service.create(machineMock.machineRequestMock());
+
+            Machine machine = service.fetch(machineCreateResponse.getId());
+            assertThat(machine.getId()).isNotNull();
+        }
+    }
+
+    @Nested
+    @DisplayName("When create and request to update an Machine")
+    class WhenCreateAndRequestToUpdateAnMachine {
+
+        @Test
+        @DisplayName("Then update it properly")
+        void thenUpdateItProperly() throws IOException {
+
+            MachineRequestMock machineMock = new MachineRequestMock();
+
+            MachineRequest machineRequest = machineMock.machineRequestMock();
+            Machine machineCreateResponse = service.create(machineRequest);
+
+
+            FakeValuesService fakeValuesService = new FakeValuesService(
+                    new Locale("pt-BR"), new RandomService());
+
+            String label = "11" + fakeValuesService.bothify("Machine????##");
+
+            machineRequest.setLabel(label);
+
+            Machine machine = service.update(machineCreateResponse.getId(), machineRequest );
+
+            assertThat(machine.getLabel()).contains(label);
+        }
+    }
+
+    @Nested
+    @DisplayName("When create and request to delete an Establishment")
+    class WhenCreateAndRequestToDeleteAnEstablishment {
+
+        @Test
+        @DisplayName("Then delete it properly")
+        void thenDeleteItProperly() {
+
+            Machine machine2 = new Machine();
+
+            try {
+
+                MachineRequestMock machineMock = new MachineRequestMock();
+
+                Machine machineCreateResponse = service.create(machineMock.machineRequestMock());
+
+                Machine machine = service.fetch(machineCreateResponse.getId());
+
+                service.delete(machine.getId());
+
+                machine2 = service.fetch(machineCreateResponse.getId());
+
+            } catch (Exception IOException){
+                assertThat(machine2.getId()).isNull();
+            }
+
+        }
+    }
 }
