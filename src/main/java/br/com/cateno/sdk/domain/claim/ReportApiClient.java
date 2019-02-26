1eb8ed3251bf63a69f526bd954b021d2f18315a7
Cainã Ferreira da Silva <caina.ferreira@sensedia.com>
2019-02-26 16:29:30 -0300

    return InputStream to ReportApi

--- src/main/java/br/com/cateno/sdk/domain/claim/ReportApiClient.java
+++ src/main/java/br/com/cateno/sdk/domain/claim/ReportApiClient.java
@@ -1,5 +1,6 @@
 package br.com.cateno.sdk.domain.claim;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.QueryMap;
@@ -9,26 +10,14 @@ import java.util.Map;
 public interface ReportApiClient {
 
     @GET("report/download.xlsx")
-    Call<String> downloadReportClaimXLSX();
-
-    @GET("report/download.xlsx")
-    Call<String> downloadReportClaimXLSX(@QueryMap Map<String, String> filters);
+    Call<ResponseBody> xlsxClaimReport(@QueryMap Map<String, String> filters);
 
     @GET("report/download.csv")
-    Call<String> downloadReportClaimCSV();
-
-    @GET("report/download.csv")
-    Call<String> downloadReportClaimCSV(@QueryMap Map<String, String> filters);
+    Call<ResponseBody> csvClaimReport(@QueryMap Map<String, String> filters);
 
     @GET("report/history.xlsx")
-    Call<String> downloadReportHistoryXLSX();
-
-    @GET("report/history.xlsx")
-    Call<String> downloadReportHistoryXLSX(@QueryMap Map<String, String> filters);
+    Call<ResponseBody> xlsxClaimHistory(@QueryMap Map<String, String> filters);
 
     @GET("report/history.csv")
-    Call<String> downloadReportHistoryCSV();
-
-    @GET("report/history.csv")
-    Call<String> downloadReportHistoryCSV(@QueryMap Map<String, String> filters);
+    Call<ResponseBody> csvClaimHistory(@QueryMap Map<String, String> filters);
 }
--- src/main/java/br/com/cateno/sdk/domain/claim/ReportService.java
+++ src/main/java/br/com/cateno/sdk/domain/claim/ReportService.java
@@ -1,11 +1,13 @@
 package br.com.cateno.sdk.domain.claim;
 
 import dagger.Reusable;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Response;
 
 import javax.inject.Inject;
 import java.io.IOException;
+import java.io.InputStream;
 
 import static dagger.internal.Preconditions.checkNotNull;
 
@@ -20,64 +22,39 @@ public class ReportService {
         this.apiClient = apiClient;
     }
 
-    public void downloadReportClaimXLSX() throws IOException {
-
-        final Call<String> call = this.apiClient.downloadReportClaimXLSX();
-        final Response<String> response = call.execute();
-        if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
-    }
-
-    public void downloadReportClaimXLSX(final ClaimFilters filters) throws IOException {
+    public InputStream downloadReportClaimXLSX(final ClaimFilters filters) throws IOException {
         checkNotNull(filters);
 
-        final Call<String> call = this.apiClient.downloadReportClaimXLSX(filters);
-        final Response<String> response = call.execute();
+        final Call<ResponseBody> call = this.apiClient.xlsxClaimReport(filters);
+        final Response<ResponseBody> response = call.execute();
         if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+        return response.body().byteStream();
     }
 
-    public void downloadReportClaimCSV() throws IOException {
-
-        final Call<String> call = this.apiClient.downloadReportClaimCSV();
-        final Response<String> response = call.execute();
-        if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
-    }
-
-    public void downloadReportClaimCSV(final ClaimFilters filters) throws IOException {
+    public InputStream downloadReportClaimCSV(final ClaimFilters filters) throws IOException {
         checkNotNull(filters);
 
-        final Call<String> call = this.apiClient.downloadReportClaimCSV(filters);
-        final Response<String> response = call.execute();
+        final Call<ResponseBody> call = this.apiClient.csvClaimReport(filters);
+        final Response<ResponseBody> response = call.execute();
         if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+        return response.body().byteStream();
     }
 
-    public void downloadReportHistoryXLSX() throws IOException {
-
-        final Call<String> call = this.apiClient.downloadReportHistoryXLSX();
-        final Response<String> response = call.execute();
-        if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
-    }
-
-    public void downloadReportHistoryXLSX(final ClaimFilters filters) throws IOException {
+    public InputStream downloadReportHistoryXLSX(final ClaimFilters filters) throws IOException {
         checkNotNull(filters);
 
-        final Call<String> call = this.apiClient.downloadReportHistoryXLSX(filters);
-        final Response<String> response = call.execute();
+        final Call<ResponseBody> call = this.apiClient.xlsxClaimHistory(filters);
+        final Response<ResponseBody> response = call.execute();
         if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+        return response.body().byteStream();
     }
 
-    public void downloadReportHistoryCSV() throws IOException {
-
-        final Call<String> call = this.apiClient.downloadReportHistoryCSV();
-        final Response<String> response = call.execute();
-        if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
-    }
-
-    public void downloadReportHistoryCSV(final ClaimFilters filters) throws IOException {
+    public InputStream downloadReportHistoryCSV(final ClaimFilters filters) throws IOException {
         checkNotNull(filters);
 
-        final Call<String> call = this.apiClient.downloadReportHistoryCSV(filters);
-        final Response<String> response = call.execute();
+        final Call<ResponseBody> call = this.apiClient.csvClaimHistory(filters);
+        final Response<ResponseBody> response = call.execute();
         if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+        return response.body().byteStream();
     }
-
 }
