400dd5ae0f3a72789c0d7af747882ca2c9e47550
Cainã Ferreira da Silva <caina.ferreira@sensedia.com>
2019-02-27 18:22:50 -0300

    machine list not exists

--- src/main/java/br/com/cateno/sdk/domain/establishment/MachineApiClient.java
+++ src/main/java/br/com/cateno/sdk/domain/establishment/MachineApiClient.java
@@ -3,15 +3,13 @@ package br.com.cateno.sdk.domain.establishment;
 import retrofit2.Call;
 import retrofit2.http.*;
 
-import java.util.List;
-
 public interface MachineApiClient {
 
     @POST("machines/")
     Call<Machine> create(@Body MachineRequest machine);
 
     @GET("machines/")
-    Call<List<Machine>> findAll();
+    Call<Machine> findByNumber(@Query("number") String number);
 
     @GET("machines/{id}")
     Call<Machine> findById(@Path("id") String id);
