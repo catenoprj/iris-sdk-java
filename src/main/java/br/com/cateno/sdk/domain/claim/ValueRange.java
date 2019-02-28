d9e3f923226c55040b2cfc8400b382c8609da89a
Tiago Angelo <tiago.angelo@sensedia.com>
2019-02-28 11:30:19 -0300

    Overloading methods

--- src/main/java/br/com/cateno/sdk/domain/claim/ValueRange.java
+++ src/main/java/br/com/cateno/sdk/domain/claim/ValueRange.java
@@ -20,6 +20,14 @@ public class ValueRange extends HashMap<String, String> {
         .ifPresent(value -> this.put("valueTo", value));
   }
 
+  public static ValueRange between(final Double from, final Double to) {
+    checkNotNull(from);
+    checkNotNull(to);
+    if (from > to)
+      throw new IllegalArgumentException("The initial value of the range cannot be greater than the final one");
+    return new ValueRange(BigDecimal.valueOf(from), BigDecimal.valueOf(to));
+  }
+
   public static ValueRange between(final BigDecimal from, final BigDecimal to) {
     checkNotNull(from);
     checkNotNull(to);
@@ -28,9 +36,19 @@ public class ValueRange extends HashMap<String, String> {
     return new ValueRange(from, to);
   }
 
+  public static ValueRange from(final Double from) {
+    checkNotNull(from);
+    return new ValueRange(BigDecimal.valueOf(from), null);
+  }
+
   public static ValueRange from(final BigDecimal from) {
     checkNotNull(from);
     return new ValueRange(from, null);
+  }
+
+  public static ValueRange to(final Double to) {
+    checkNotNull(to);
+    return new ValueRange(null, BigDecimal.valueOf(to));
   }
 
   public static ValueRange to(final BigDecimal to) {
